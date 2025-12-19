41898d2a248444d0e9c504c424bdbc61d5b67d78
deekshithsch25-netizen <deekshiths.ch25@bmsce.ac.in>
2025-12-19 08:09:48 +0530

    output.c

---
+++ output.c
@@ -0,0 +1,87 @@
+
+===== Student Management System =====
+1. Add Student
+2. Find by Roll Number
+3. Find by First Name
+4. Find by Course
+5. Update Student
+6. Delete Student
+7. Count Students
+0. Exit
+Enter your choice: 1
+Enter Roll Number: 1
+Enter First Name: su
+Enter Last Name: shan
+Enter Course: chy
+Student added successfully!
+
+===== Student Management System =====
+1. Add Student
+2. Find by Roll Number
+3. Find by First Name
+4. Find by Course
+5. Update Student
+6. Delete Student
+7. Count Students
+0. Exit
+Enter your choice: 1
+Enter Roll Number: 2
+Enter First Name: jay
+Enter Last Name: anth
+Enter Course: eee
+Student added successfully!
+
+===== Student Management System =====
+1. Add Student
+2. Find by Roll Number
+3. Find by First Name
+4. Find by Course
+5. Update Student
+6. Delete Student
+7. Count Students
+0. Exit
+Enter your choice: 2
+Enter Roll Number to search: 1
+Found:
+Roll: 1
+Name: su shan
+Course: chy
+
+===== Student Management System =====
+1. Add Student
+2. Find by Roll Number
+3. Find by First Name
+4. Find by Course
+5. Update Student
+6. Delete Student
+7. Count Students
+0. Exit
+Enter your choice: 3
+Enter First Name: su
+Roll: 1 | Name: su shan | Course: chy
+
+===== Student Management System =====
+1. Add Student
+2. Find by Roll Number
+3. Find by First Name
+4. Find by Course
+5. Update Student
+6. Delete Student
+7. Count Students
+0. Exit
+Enter your choice: 4
+Enter Course: eee
+Roll: 2 | Name: jay anth | Course: eee
+
+===== Student Management System =====
+1. Add Student
+2. Find by Roll Number
+3. Find by First Name
+4. Find by Course
+5. Update Student
+6. Delete Student
+7. Count Students
+0. Exit
+Enter your choice: 6
+Enter Roll Number to delete: 1
+Student deleted successfully!
